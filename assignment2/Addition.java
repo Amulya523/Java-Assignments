7562afc13149d68b0afea0f0fdc12a9a76c01a88
Amulya523 <162595425+Amulya523@users.noreply.github.com>
2024-05-31 16:54:48 +0530

    Add files via upload

---
+++ assignment2/Addition.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+public class Addition {
+
+	public static void main(String[] args) 
+	{
+		int a, b, sum;
+		a=10;
+		b=10;
+		sum = a+b;
+		System.out.println("Sum of a,b is 10+10:" +sum);
+	}
+
+}
---
+++ assignment2/Name.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+public class Name {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Amulya");
+		System.out.println("Amulya");
+		System.out.println("Amulya");
+		System.out.println("Amulya");
+		System.out.println("Amulya");
+		
+
+	}
+
+}
